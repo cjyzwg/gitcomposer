a0d66901cbaba3e497c49690b0d0b156b403a8f7
cjyzwg <1044035278@qq.com>
2020-04-10 20:43:46 +0800

    first

---
+++ hi.php
@@ -0,0 +1,4 @@
+<?php
+require_once './vendor/autoload.php';
+use Tools\Tools;
+echo Tools::hi();
---
+++ src/Tools/Tools.php
@@ -0,0 +1,10 @@
+<?php
+namespace Tools;
+class Tools
+{
+    public function __construct() {
+    }
+    public static function hi() {
+        echo 'hi';
+    }
+}
